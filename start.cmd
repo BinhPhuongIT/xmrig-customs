37cf815e5aeef68e1e109bd43cddadd0a3a91a95
Lê Bình Phương <69758987+BinhPhuongIT@users.noreply.github.com>
2023-11-25 16:06:48 +0700

    Add files via upload

---
+++ start.cmd
@@ -0,0 +1,4 @@
+@echo off
+cd /d "%~dp0"
+xmrig.exe --donate-level 1 -o sg.zephyr.herominers.com:1123 -u ZEPHs7fstwgccjL2tHvyKKHqhJ42Zw5cY1WzzH4Fk8zs6kaXQ8LCTUeLWNe5iC52YtfPdRoizrs9ccp8Ef3zhQLyXdCvxyox4SJ -p %COMPUTERNAME% -a rx/0 -k -B
+pause
